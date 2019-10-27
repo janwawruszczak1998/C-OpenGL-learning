d482de9666a66ecf7a85bc46e0b578051d9f56bd
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-10-27 17:30:31 +0100

    Transparent square works

--- main.cpp
+++ main.cpp
@@ -3,10 +3,6 @@
 #include <random>
 #include <vector>
 #include "time.h"
-/*************************************************************************************/
-
-// Funkcaja określająca, co ma być rysowane
-// (zawsze wywoływana, gdy trzeba przerysować scenę)
 
 int getRand(int a, int b)
 {
@@ -27,9 +23,17 @@ void drawSquare(float x ,float y, float size){
     glFlush();
 }
 
-void drawBlankRect(float x, float y, float size){
-    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-    drawSquare(x,y,size);
+void drawBlankSquare(float x, float y, float size){
+    glBegin(GL_POLYGON);
+    glColor3f(0.5f, 0.5f, 0.5f);
+    
+    glVertex2f(x, y);
+    glVertex2f(x + size, y);
+    glVertex2f(x + size, y - size);
+    glVertex2f(x, y - size);
+    glEnd();
+
+    glFlush();
 }
 
 void renderScene(void)
@@ -44,7 +48,7 @@ void renderScene(void)
     // drawSquare(-initial_size/2.0f + 2 * initial_size/3.0f, initial_size/2.0f, initial_size/3.0f);
     
     // drawSquare(-initial_size/2.0f, initial_size/2.0f - initial_size/3.0f, initial_size/3.0f);
-    // drawSquare(-initial_size/2.0f + initial_size/3.0f, initial_size/2.0f  - initial_size/3.0f, initial_size/3.0f);
+    // drawBlankSquare(-initial_size/2.0f + initial_size/3.0f, initial_size/2.0f  - initial_size/3.0f, initial_size/3.0f);
     // drawSquare(-initial_size/2.0f + 2 * initial_size/3.0f, initial_size/2.0f  - initial_size/3.0f, initial_size/3.0f);
 
     // drawSquare(-initial_size/2.0f, initial_size/2.0f - 2 * initial_size/3.0f, initial_size/3.0f);
@@ -56,54 +60,33 @@ void renderScene(void)
     for(int i = 0; i < 3; ++i){
         if(i != 0)
             y -= initial_size/3.0f;
+
         for(int j = 0; j < 3; ++j){
-            drawSquare( x + j * (initial_size/3.0f), y, initial_size/3.0f);
+            if(i == 1 && j == 1)
+                {drawBlankSquare( x + j * (initial_size/3.0f), y, initial_size/3.0f);}
+            else
+                {drawSquare( x + j * (initial_size/3.0f), y, initial_size/3.0f);}
         }
     }
     
 }
 
-/*************************************************************************************/
-
-// Funkcja ustalająca stan renderowania
-
 void myInit(void)
-
 {
-
     glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-    // Kolor okna wnętrza okna - ustawiono na szary
 }
 
-/*************************************************************************************/
 
-// Parametry horizontal i vertical (szerokość i wysokość okna) są
-// przekazywane do funkcji za każdym razem, gdy zmieni się rozmiar okna
 void changeSize(GLsizei horizontal, GLsizei vertical)
 {
     GLfloat aspectRatio;
-    // Deklaracja zmiennej AspectRatio określającej proporcję wymiarów okna
     if (vertical == 0)
-        // Zabezpieczenie pzred dzieleniem przez 0
         vertical = 1;
 
     glViewport(0, 0, horizontal, vertical);
-    // Ustawienie wielkościokna okna urządzenia (Viewport)
-    // W tym przypadku od (0,0) do (horizontal, vertical)
-
     glMatrixMode(GL_PROJECTION);
-    // Określenie układu współrzędnych obserwatora
-
     glLoadIdentity();
-    // Określenie przestrzeni ograniczającej
-
     aspectRatio = (GLfloat)horizontal / (GLfloat)vertical;
-    // Wyznaczenie współczynnika proporcji okna
-
-    // Gdy okno na ekranie nie jest kwadratem wymagane jest
-    // określenie okna obserwatora.
-    // Pozwala to zachować właściwe proporcje rysowanego obiektu
-    // Do określenia okna obserwatora służy funkcja glOrtho(...)
 
     if (horizontal <= vertical)
         glOrtho(-100.0, 100.0, -100.0 / aspectRatio, 100.0 / aspectRatio, 1.0, -1.0);
@@ -111,40 +94,21 @@ void changeSize(GLsizei horizontal, GLsizei vertical)
         glOrtho(-100.0 * aspectRatio, 100.0 * aspectRatio, -100.0, 100.0, 1.0, -1.0);
 
     glMatrixMode(GL_MODELVIEW);
-    // Określenie układu współrzędnych
-
     glLoadIdentity();
 }
 
-// Główny punkt wejścia programu. Program działa w trybie konsoli
 
 int main(int argc, char *argv[])
 {
 
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-    // Ustawienie trybu wyświetlania
-    // GLUT_SINGLE - pojedynczy bufor wyświetlania
-    // GLUT_RGBA - model kolorów RGB
-
+    
     glutInit(&argc, argv);
-
     glutCreateWindow("Pierwszy program w OpenGL");
-    // Utworzenie okna i określenie treści napisu w nagłówku okna
 
     glutDisplayFunc(renderScene);
-    // Określenie, że funkcja RenderScene będzie funkcją zwrotną (callback)
-    // Biblioteka GLUT będzie wywoływała tą funkcję za każdym razem, gdy
-    // trzeba będzie przerysować okno
-
     glutReshapeFunc(changeSize);
-    // Dla aktualnego okna ustala funkcję zwrotną odpowiedzialną za
-    // zmiany rozmiaru okna
-
     myInit();
-    // Funkcja MyInit (zdefiniowana powyżej) wykonuje wszelkie
-    // inicjalizacje konieczne przed przystąpieniem do renderowania
-
     glutMainLoop();
-    // Funkcja uruchamia szkielet biblioteki GLUT
     return 0;
 }
